cb7c704c2a82812d5e11a22c2d33b496ae971c8b
GuilhermeGSartori <guigsartori@hotmail.com>
2023-06-12 20:28:13 -0300

    Removed comments from the test file

--- src/etapa2/_test_code_gui.c
+++ src/etapa2/_test_code_gui.c
@@ -1,31 +1,12 @@
 int exemplo;
 bool exemplo, _exemplo;
 
-/*fun () -> int {
-    int a;
-    int a, b, c; //ver se posso atribuir literal
-    int b
-    //int _a = 1; //-> realmente nao eh assim?
-    int _a <= 1;
-    //float d = 10.1; //precisa manter a conssitencia? gramaticamente faz sentido, questao de semantica vemos dps
-    if(a) {
-        int e;
-    };
-
-    if(b) {
-    };
-    else {
-        int f;
-    };
-
-};*/
-
 main () -> int {
     int a;
-    int a, b, c; //ver se posso atribuir literal
+    int a, b, c; 
     int b;
     int _a <= 1;
-    float d <= 10.1; //precisa manter a conssitencia? gramaticamente faz sentido, questao de semantica vemos dps
+    float d <= 10.1;
     {};
     if(a) {};
     if(a) {
@@ -39,13 +20,7 @@ main () -> int {
         float w;
     };
 
-    if(b) {
-        //int j;
-    }; // -> CONSERTAR ISSO
-      // DEVIA TER UM PONTO VIRGULA AQUI... ESTOU TENDO PROBLEMA COM SHIFT REDUCE... RESOLVER ISSO DE MANEIRA INTELIGENTE
-      // O problema eh que um if pode sim acabar com ponto virgula, e ate onde o bison sabe, o else e o if sao separados
-      // logo, eh possivel acontecer um if por ser ele mesmo (com ponto virgula no final) e um if com ponto vigula nao
-      // sabendo se veio do else ou nao
+    if(b) {};
     else {
         int f;
     };
@@ -65,9 +40,3 @@ main () -> int {
     return a;
 
 };
-
-//foo (int input) -> bool a
-
-//foo (int input, bool _input) -> bool a
-
-//faa (int input, bool _input) -> bool output a
